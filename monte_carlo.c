7f0155711efe7d9a171904783a77352f3a0db29f
Saranjosh <84576650+Saranjosh@users.noreply.github.com>
2021-06-03 21:38:52 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -6,7 +6,7 @@
 float mc_pi(int);
 
 float frandom() {
-  long int q = frandom();
+  long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
@@ -14,11 +14,11 @@ float frandom() {
 int main(void) {
   float pi0;
   float pi1;
-
+  
   pi0 = mc_pi(25000);
   pi1 = mc_pi(25000);
   printf("%f %f\n", pi0, pi1);
-
+  
   if (pi0 == pi1) {
       printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
       abort();
@@ -29,7 +29,7 @@ int main(void) {
       abort();
   }
 
-
+    
   for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
     if (!(fabs(pi0 - M_PI) < 0.4)) {
@@ -39,18 +39,37 @@ int main(void) {
   }
 }
 float mc_pi(int n){
-float x,y;
-int area_of_circle=0;
-int area_of_square=0;
-for(int i=0;i<n;i++){
-        x= frandom();
-        y= frandom();
-        float z= x*x + y*y;
-    if(z<=1){
-      area_of_circle+=1;
-      area_of_square+=1;
-    }
-}
-float pi = (float)4*area_of_circle/area_of_square;
-  return pi;
+  float x,y;
+  int i=0,circle=0;
+  float z;
+  int square=0;
+  
+  float pi;
+  
+  while (i<n-1){
+   
+   x=frandom();
+   y=frandom();
+   z=x*x+y*y;
+  
+   
+   if(z<=1){
+     circle+=1;
+     square+=1;
+     }
+   else{
+     square+=1;
+   
+   i++;
+     
+   }
+   
+   }
+ 
+ pi=(float)4*circle/square;
+ return pi;
+   
+    
+  
+  
 }
