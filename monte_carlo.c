dad297e5dba80d5ff813a471e878d94bb4ae764e
Saranjosh <84576650+Saranjosh@users.noreply.github.com>
2021-06-03 21:28:54 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -14,11 +14,11 @@ float frandom() {
 int main(void) {
   float pi0;
   float pi1;
-  
+
   pi0 = mc_pi(25000);
   pi1 = mc_pi(25000);
   printf("%f %f\n", pi0, pi1);
-  
+
   if (pi0 == pi1) {
       printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
       abort();
@@ -29,7 +29,7 @@ int main(void) {
       abort();
   }
 
-    
+
   for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
     if (!(fabs(pi0 - M_PI) < 0.4)) {
@@ -38,15 +38,14 @@ int main(void) {
     }
   }
 }
-
 float mc_pi(int n){
 float x,y;
-x= frandom();
-y= frandom();
 int area_of_circle=0;
 int area_of_square=0;
-float z= x*x + y*y;
 for(int i=0;i<n;i++){
+        x= frandom();
+        y= frandom();
+        float z= x*x + y*y;
     if(z<=1){
       area_of_circle+=1;
       area_of_square+=1;
@@ -54,4 +53,3 @@ for(int i=0;i<n;i++){
 }
 float pi = (float)4*area_of_circle/area_of_square;
 }
-
